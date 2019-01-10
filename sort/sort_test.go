4fd131daf6b6b1167c60de246d3fac8b2235a38c
Gildas Chabot <gildas.chabot@schibsted.com>
2019-01-10 13:28:08 +0100

    add benchmarks on lists of size 100k

--- sort/sort_test.go
+++ sort/sort_test.go
@@ -37,3 +37,36 @@ func BenchmarkStdSort(b *testing.B) {
 		sort.Slice(a, func(i, j int) bool { return a[i] < a[j] })
 	}
 }
+
+func BenchmarkQuickSort100000(b *testing.B) {
+
+	for i := 0; i < b.N; i++ {
+		b.StopTimer()
+		a := randomSlice(100000)
+		b.StartTimer()
+
+		QuickSort(a)
+	}
+}
+
+func BenchmarkMergeSort100000(b *testing.B) {
+
+	for i := 0; i < b.N; i++ {
+		b.StopTimer()
+		a := randomSlice(100000)
+		b.StartTimer()
+
+		MergeSort(a)
+	}
+}
+
+func BenchmarkStdSort100000(b *testing.B) {
+
+	for i := 0; i < b.N; i++ {
+		b.StopTimer()
+		a := randomSlice(100000)
+		b.StartTimer()
+
+		sort.Slice(a, func(i, j int) bool { return a[i] < a[j] })
+	}
+}
